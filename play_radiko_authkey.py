98b232c837d4c2089d1c8d6a82633e7d5673fba9
takuya <takuya.1st+nospam@gmail.com>
2017-09-19 17:35:44 +0900

    キーだけ取り出せるように

---
+++ play_radiko_authkey.py
@@ -0,0 +1,23 @@
+from play_radiko import Radiko
+import argparse
+import datetime
+import subprocess
+
+
+def main():
+  
+  
+  parser = argparse.ArgumentParser(description=u'radiko用のキーを取得して出力する')
+  
+  radiko = Radiko()
+  authtoken = radiko.auth_key()
+  #
+  print(f'radiko_token: {authtoken}')
+
+
+
+if __name__ == '__main__':
+  main()
+
+
+
