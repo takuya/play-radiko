ebf350fac27dbddefdb2d44ff0381a9e4991b0d5
takuya <takuya.1st+nospam@gmail.com>
2017-09-22 23:11:22 +0900

    コマンドオプション

--- play_radiko.py
+++ play_radiko.py
@@ -10,10 +10,10 @@ import subprocess
 
 
 ## for debugging
-# import IPython
-# import pprint
-# pp = pprint.PrettyPrinter(indent=4)
-# pprint = pp.pprint
+import IPython
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+pprint = pp.pprint
 
 def main():
 
@@ -22,7 +22,7 @@ def main():
 
   parser.add_argument('channel_name', help='チャンネル' )
   parser.add_argument('-d', '--duration',default='1800' , help='再生（録音）時間', type=int)
-  parser.add_argument('-o', '--output'  , help='保存先')
+  parser.add_argument('-o', '--output'  , help='保存先',nargs='?', default=None,const='')
   parser.add_argument('--no-play-live',action='store_const',default=False,const=True, help='再生しない')
   
   
@@ -32,16 +32,22 @@ def main():
   #
   radiko = Radiko()
 
+
+
+  pprint( vars(args)['output'] )
   ## 
-  if vars(args)['no_play_live'] == False :
+  if vars(args)['no_play_live'] == True :
+    print('only save ')
     f_out = vars(args)['output']
     radiko.save_radiko(channel,duration,output=f_out)
     exit()
   ##
-  if vars(args)['output'] :
-    radiko.save_and_play_radiko(channel,duration, output=vars(args)['output'])
+  if vars(args)['output'] != None :
+    print('live and save ')
+    radiko.play_and_save_radiko(channel,duration, output=vars(args)['output'])
     exit()
   else:
+    print('only -live')
     radiko.play_radiko(channel,duration)
     exit()
 
