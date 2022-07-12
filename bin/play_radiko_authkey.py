6e7a7c75463932dbb831dc21128aca0bb30b480e
takuya <55338+takuya@users.noreply.github.com>
2022-07-12 18:58:23 +0900

    コマンドを整理

--- bin/play_radiko_authkey.py
+++ bin/play_radiko_authkey.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
-import argparse
 
 
-from RadikoPlay.RadikoAuth import RadikoAuth
 
 
 def main():
+  from RadikoPlay.RadikoAuth import RadikoAuth
+  import argparse
   parser = argparse.ArgumentParser(description='radiko用のキーを取得して出力する')
 
   auth = RadikoAuth()
   auth.access_auth()
   token = auth.auth_token
   #
-  print(f'radiko_token: {token}')
+  print(f'radiko_token={token}')
 
 
 if __name__ == '__main__':
+  import sys,os
+  sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
   main()
